e00cc648e06c7d35a0f439199a47d30d34c09798
yumimint <i.yumimint@gmail.com>
2021-09-14 04:01:36 +0900

    socket version

--- bouyomichan.py
+++ bouyomichan.py
@@ -1,13 +1,25 @@
 # bouyomichan
-# Text to speech interface for BouyomiChan via http.
-import json
+# Text to speech interface for BouyomiChan via socket.
+import enum
 import queue
+import socket
+import struct
 import threading
-import urllib.request
-from urllib.error import URLError
 
-DEFAULT_HOST = 'localhost:50080'
+DEFAULT_HOST = ('localhost', 50001)
+
 _talk_q = queue.Queue()
+
+
+class Cmd(enum.IntEnum):
+    Talk = 0x0001
+    Pause = 0x0010
+    Resume = 0x0020
+    Skip = 0x0030
+    Clear = 0x0040
+    GetPause = 0x0110
+    GetNowPlaying = 0x0120
+    GetTaskCount = 0x0130
 
 
 def talk(text, voice=None, volume=None, speed=None, tone=None, host=None):
@@ -18,117 +30,73 @@ def talk(text, voice=None, volume=None, speed=None, tone=None, host=None):
 def _talk_daemon():
     while True:
         text, voice, volume, speed, tone, host = _talk_q.get()
-        text = urllib.parse.quote(text)
-        post = len(text) >= 1000  # textが長すぎるときはPOSTメソッドを使う
-        host = DEFAULT_HOST if host is None else host
-        params = {}
-        if post:
-            data = f"text={text}".encode("ascii")
-        else:
-            params["text"] = text
-        if voice is not None:
-            params["voice"] = voice
-        if volume is not None:
-            params["volume"] = volume
-        if speed is not None:
-            params["speed"] = speed
-        if tone is not None:
-            params["tone"] = tone
-        q = "&".join(map(lambda x: f"{x[0]}={str(x[1])}", params.items()))
-        url = f'http://{host}/talk?{q}'
+        cmdid = Cmd.Talk
+        voice = voice or 0
+        volume = volume or -1
+        speed = speed or -1
+        tone = tone or -1
+        code = 0  # 0:UTF-8, 1:Unicode, 2:Shift-JIS
+        message = bytes(text, "utf-8")
+        length = len(message)
+        data = struct.pack("<hhhhhbL", cmdid, speed, tone,
+                           volume, voice, code, length) + message
         try:
-            if post:
-                # POSTするとparamsが反映されない?
-                req = urllib.request.Request(url, data)
-            else:
-                req = urllib.request.Request(url)
-            urllib.request.urlopen(req)
-        except URLError:  # 棒読みちゃんが起動していない
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect(host or DEFAULT_HOST)
+                sock.sendall(data)
+        except socket.error:  # 棒読みちゃんが起動していない
             while not _talk_q.empty():
                 _talk_q.get_nowait()
 
 
-def _cmd(command, host=None):
-    host = DEFAULT_HOST if host is None else host
-    url = f'http://{host}/{command}'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as res:
-        obj = json.load(res)
-    vals = list(obj.values())
-    if vals:
-        return vals[0] if len(vals) == 1 else vals
+def _cmd(host, cmdid: int, res_len=0):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect(host or DEFAULT_HOST)
+            sock.sendall(struct.pack("<h", cmdid))
+            if res_len > 0:
+                buf = sock.recv(res_len)
+                return int.from_bytes(buf, "little")
+
+    except socket.error:
+        return None
+
+    return None
 
 
 def pause(host=None):
     """一時停止する"""
-    try:
-        _cmd("pause", host)
-    except URLError:
-        pass
+    return _cmd(host, Cmd.Pause)
 
 
 def resume(host=None):
     """一時停止を解除する"""
-    try:
-        _cmd("resume", host)
-    except URLError:
-        pass
+    return _cmd(host, Cmd.Resume)
 
 
 def skip(host=None):
     """現在の行をスキップして次の行へ"""
-    try:
-        _cmd("skip", host)
-    except URLError:
-        pass
+    return _cmd(host, Cmd.Skip)
 
 
 def clear(host=None):
     """全ての行をキャンセルする"""
-    try:
-        _cmd("clear", host)
-    except URLError:
-        pass
+    return _cmd(host, Cmd.Clear)
 
 
 def getpause(host=None):
     """一時停止中かどうかを取得する"""
-    try:
-        return _cmd("getpause", host)
-    except URLError:
-        return None
+    return _cmd(host, Cmd.GetPause, 1)
 
 
 def getnowplaying(host=None):
     """音声再生中かどうかを取得する"""
-    try:
-        return _cmd("getnowplaying", host)
-    except URLError:
-        return None
-
-
-def getnowtaskid(host=None):
-    """現在のタスク番号を取得する"""
-    try:
-        return _cmd("getnowtaskid", host)
-    except URLError:
-        return None
+    return _cmd(host, Cmd.GetNowPlaying, 1)
 
 
 def gettalktaskcount(host=None):
     """残りのタスク数を取得する"""
-    try:
-        return _cmd("gettalktaskcount", host)
-    except URLError:
-        return None
-
-
-def getvoicelist(host=None):
-    """利用可能な音声合成エンジンの一覧を取得する"""
-    try:
-        return _cmd("getvoicelist", host)
-    except URLError:
-        return None
+    return _cmd(host, Cmd.GetTaskCount, 4)
 
 
 _talkth = threading.Thread(target=_talk_daemon, daemon=True)
